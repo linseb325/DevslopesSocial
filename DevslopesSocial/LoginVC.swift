c78eae2a9b88776a9041e0211d0380eb2f022a0a
linseb325 <linseb325@gmail.com>
2017-06-22 15:21:48 -0500

    Initial commit. Added Firebase pods.

---
+++ DevslopesSocial/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  DevslopesSocial
+//
+//  Created by Brennan Linse on 6/22/17.
+//  Copyright © 2017 Brennan Linse. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
