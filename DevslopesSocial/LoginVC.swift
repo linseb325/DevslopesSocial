d076972184f90e6a6e54be1f5a347198d4b7a735
linseb325 <linseb325@gmail.com>
2017-06-26 18:25:32 -0500

    Added Firebase e-mail/password authentication.

--- DevslopesSocial/LoginVC.swift
+++ DevslopesSocial/LoginVC.swift
@@ -10,12 +10,19 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
+import FirebaseAuth
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: FancyTextField!
+    @IBOutlet weak var passwordTextField: FancyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,11 +36,11 @@ class LoginVC: UIViewController {
         
         facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
             if error != nil {
-                print("ERROR: Unable to authenticate with Facebook: \(error.debugDescription)")
+                print("Brennan - Unable to authenticate with Facebook: \(error.debugDescription)")
             } else if result?.isCancelled == true {
-                print("User cancelled Facebook authentication")
+                print("Brennan - User cancelled Facebook authentication")
             } else {
-                print("Successfully authenticated with Facebook")
+                print("Brennan - Successfully authenticated with Facebook")
                 let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
                 self.authenticateFirebase(credential)
             }
@@ -43,16 +50,37 @@ class LoginVC: UIViewController {
     func authenticateFirebase(_ credential: AuthCredential) {
         Auth.auth().signIn(with: credential) { (user, error) in
             if error != nil {
-                print("ERROR: Unable to authenticate with Firebase: \(error.debugDescription)")
+                print("Brennan - ERROR: Unable to authenticate with Firebase: \(error.debugDescription)")
             } else {
-                print("Successfully authenticated with Firebase")
+                print("Brennan - Successfully authenticated with Firebase in 'authenticateFirebase' method")
             }
         }
     }
     
     
+    @IBAction func signInPressed(_ sender: FancyButton) {
+        if let emailAddress = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: emailAddress, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("Brennan - Email user authenticated with Firebase")
+                } else {
+                    // Try to create a new user
+                    Auth.auth().createUser(withEmail: emailAddress, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("Brennan - Unable to create new user with Firebase: \(error.debugDescription)")
+                        } else {
+                            print("Brennan - Successfully created new user with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
     
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
     
     
     
